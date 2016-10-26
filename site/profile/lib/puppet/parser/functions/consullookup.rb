dd533ddba8006b12798b2a1fd3ae71909a756e8d
Nicolas Corrarello <ncorrare@gmail.com>
2016-10-26 22:29:40 +0100

    Fact name for lookupvar should be a string

--- site/profile/lib/puppet/parser/functions/consullookup.rb
+++ site/profile/lib/puppet/parser/functions/consullookup.rb
@@ -1,7 +1,7 @@
 module Puppet::Parser::Functions
   newfunction(:consullookup, :type => :rvalue, :arity => 1) do |args|
-      consulserver = lookupvar(consulserver)
-      Resolv::DNS.new(:nameserver_port => [[consulserver,8600]],
+      consulserver = lookupvar('consulserver')
+      A = Resolv::DNS.new(:nameserver_port => [[consulserver,8600]],
                 :ndots => 1)
       ip = A.getaddress(args[0])
       ip.to_s

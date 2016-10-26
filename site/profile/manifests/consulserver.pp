b0f1c77dbfe64cecf9c08c5b41b131e2545da8de
Nicolas Corrarello <ncorrare@gmail.com>
2016-10-26 12:46:00 +0100

    Adding consulserver profile

---
+++ site/profile/manifests/consulserver.pp
@@ -0,0 +1,14 @@
+class profile::consulserver {
+  class { '::consul':
+    config_hash => {
+      'bootstrap_expect' => 1,
+      'client_addr'      => '0.0.0.0',
+      'data_dir'         => '/opt/consul',
+      'datacenter'       => 'aws',
+      'log_level'        => 'INFO',
+      'node_name'        => $::fqdn,
+      'server'           => true,
+      'ui_dir'           => '/opt/consul/ui',
+    }
+  }
+}

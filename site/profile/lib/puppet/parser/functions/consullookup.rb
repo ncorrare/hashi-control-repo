112083e911c038e28edb40ac67b7394d4f7d89e4
Nicolas Corrarello <ncorrare@gmail.com>
2016-10-26 22:03:30 +0100

    Minor refactor to consullookup function

--- site/profile/lib/puppet/parser/functions/consullookup.rb
+++ site/profile/lib/puppet/parser/functions/consullookup.rb
@@ -2,7 +2,8 @@ module Puppet::Parser::Functions
   newfunction(:consullookup, :type => :rvalue, :arity => 1) do |args|
       consulserver = lookupvar(consulserver)
       Resolv::DNS.new(:nameserver_port => [[consulserver,8600]],
-                :search => [args[0]],
                 :ndots => 1)
+      ip = A.getaddress(args[0])
+      ip.to_s
   end
 end

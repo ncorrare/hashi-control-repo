7144abb11c3895f94018897459c23ba3e52006c7
Nicolas Corrarello <ncorrare@gmail.com>
2016-10-30 11:03:53 +0000

    Changed for a null provisioner in terraform which seems way smarter

--- site/profile/manifests/database.pp
+++ site/profile/manifests/database.pp
@@ -17,13 +17,6 @@ class profile::database {
     user       => 'vault@%',
     notify     => Exec['provision-password'],
   }
-  #This is a rather hacky way to provision the database password. Basically this script would be waiting for the vault server to be available and give it the password (which we assume it will happen within a minute of this server being available. Don't do this at home, basically.
-  exec { 'provision-password':
-    command   => '/bin/curl https://raw.githubusercontent.com/ncorrare/terraform-examples/master/setmysqlpassword.sh | /bin/bash',
-    tries     => 6,
-    try_sleep => 10,
-    require   => Class['consul'],
-  }
 
   class { '::consul':
     config_hash => {

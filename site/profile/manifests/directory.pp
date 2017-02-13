2a97695dba95410afab2d2bc2f1829f64adb4f56
Nicolas Corrarello <nicolas@corrarello.com>
2017-02-13 09:13:06 -0300

    Added profile for a base directory configuration

---
+++ site/profile/manifests/directory.pp
@@ -0,0 +1,9 @@
+class profile::directory {
+  include profile::base
+  class { 'openldap::server': }
+  openldap::server::database { 'dc=example,dc=com':
+    directory => '/var/lib/ldap',
+    rootdn    => 'cn=admin,dc=example,dc=com',
+    rootpw    => 'hashicorp',
+  }
+}

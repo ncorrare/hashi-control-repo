f00582083ff91b30e273a71524a1d810d1ff5d3e
Nicolas Corrarello <nicolas@corrarello.com>
2017-03-02 23:13:58 +0000

    Minor adjustments

--- site/profile/files/generatecert.sh
+++ site/profile/files/generatecert.sh
@@ -2,13 +2,13 @@
 set -e
 cd /usr/share/easy-rsa/2.0/
 source ./vars
-./clean-all
-./build-ca --batch
-./build-dh --batch
-./build-key-server --batch $(hostname)
+/usr/share/easy-rsa/2.0/clean-all
+/usr/share/easy-rsa/2.0/build-ca --batch
+/usr/share/easy-rsa/2.0/build-dh --batch
+/usr/share/easy-rsa/2.0/build-key-server --batch $(hostname)
 mkdir -p /etc/vault
-cp keys/ca.crt /etc/vault
-cp keys/$(hostname).* /etc/vault
+cp /usr/share/easy-rsa/2.0/keys/ca.crt /etc/vault
+cp /usr/share/easy-rsa/2.0/keys/$(hostname).* /etc/vault
 chown -R vault:vault /etc/vault
 cp /etc/vault/ca.crt /etc/pki/ca-trust/source/anchors/
 /bin/update-ca-trust

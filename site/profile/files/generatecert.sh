3865fd547890923d4bbfe69613d6d0934749c96e
Nicolas Corrarello <nicolas@corrarello.com>
2017-03-10 12:21:01 -0800

    Copy ca cert to host

--- site/profile/files/generatecert.sh
+++ site/profile/files/generatecert.sh
@@ -11,4 +11,5 @@ cp /usr/share/easy-rsa/2.0/keys/ca.crt /etc/ssl/vault
 cp /usr/share/easy-rsa/2.0/keys/$(hostname).* /etc/ssl/vault
 chown -R vault:vault /etc/ssl/vault
 cp /etc/ssl/vault/ca.crt /etc/pki/ca-trust/source/anchors/
+cp /etc/ssl/vault/ca.crt /vagrant/
 /bin/update-ca-trust

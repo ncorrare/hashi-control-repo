80bec79bce83a0b77f51bcedcd97709b203a3e40
Nicolas Corrarello <nicolas@corrarello.com>
2017-03-14 17:28:58 -0500

    Removed path to update-ca-trust

--- site/profile/files/generatecert.sh
+++ site/profile/files/generatecert.sh
@@ -11,4 +11,4 @@ cp /usr/share/easy-rsa/2.0/keys/ca.crt /etc/ssl/vault
 cp /usr/share/easy-rsa/2.0/keys/$(hostname).* /etc/ssl/vault
 chown -R vault:vault /etc/ssl/vault
 cp /etc/ssl/vault/ca.crt /etc/pki/ca-trust/source/anchors/
-/bin/update-ca-trust
+update-ca-trust

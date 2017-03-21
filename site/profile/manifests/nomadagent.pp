0c579ae07e741f83fdcb0c7655fbc54def7cbee1
Nicolas Corrarello <nicolas@corrarello.com>
2017-03-21 09:43:57 +0000

    Added nomadagent class

---
+++ site/profile/manifests/nomadagent.pp
@@ -0,0 +1,18 @@
+class profile::nomadagent {
+  class { 'nomad':
+    version       => '0.5.5',
+    config_hash   => {
+      'region'     => 'uk',
+      'datacenter' => 'poundhost',
+      'log_level'  => 'INFO',
+      'bind_addr'  => '0.0.0.0',
+      'data_dir'   => '/opt/nomad',
+      'client'     => {
+        'enabled'    => true,
+        'servers'    => [
+          "nomadserver.corrarello.com:4647"
+        ]
+      }
+    },
+  }
+}
